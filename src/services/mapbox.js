c97828d23cb646b2fcadca4b2dca888039f38440
Adam Tomas <adahy.to@gmail.com>
2021-02-20 17:37:16 +0100

    kill the master

--- src/services/mapbox.js
+++ src/services/mapbox.js
@@ -1,7 +1,7 @@
 import mapbox from 'mapbox-gl';
 
 // https://docs.mapbox.com/help/glossary/access-token/
-mapbox.accessToken = "";
+mapbox.accessToken = "pk.eyJ1IjoiYWRhaHl0byIsImEiOiJja2RkMGh3eGQwNHJrMnF1Y21la3RsZHlmIn0.JDnphYptyZrl1stuPEsnZQ";
 
 const key = {};
 
